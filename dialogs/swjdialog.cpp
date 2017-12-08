d4ba67ce8713e8617272a227fe150d1d6bef4c38
anton <vao@asu-vei.ru>
2017-12-08 17:04:14 +0300

    2.1.205
    temporary

---
+++ dialogs/swjdialog.cpp
@@ -0,0 +1,6 @@
+#include "swjdialog.h"
+
+SWJDialog::SWJDialog()
+{
+
+}
---
+++ dialogs/swjdialog.h
@@ -0,0 +1,11 @@
+#ifndef SWJDIALOG_H
+#define SWJDIALOG_H
+
+
+class SWJDialog : public QDialog
+{
+public:
+    SWJDialog();
+};
+
+#endif // SWJDIALOG_H
