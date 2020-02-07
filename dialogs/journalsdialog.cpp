1c79faaba177050afa7a885a3963100914cabd06
SAU <sau@asu-vei.ru>
2020-02-07 18:20:52 +0300

    Измерительный журнал

--- dialogs/journalsdialog.cpp
+++ dialogs/journalsdialog.cpp
@@ -380,9 +380,9 @@ void JournalDialog::FillWorkJour(QVector<S2::DataRec>* File)
                 memcpy(&N, &System.EvNum, sizeof(System.EvNum));
                 N = N&0x00FFFFFF;
 
-                if(N <= Discription.size())
+                if((N-3010) <= Discription.size())
                 {
-                  Num << Discription.at(N);
+                  Num << Discription.at(N-3011);
                 }
                 else
                 {
@@ -415,9 +415,9 @@ void JournalDialog::FillWorkJour(QVector<S2::DataRec>* File)
                     memcpy(&N, &System.EvNum, sizeof(System.EvNum));
                     N = N&0x00FFFFFF;
 
-                    if(N <= Discription.size())
+                    if((N-3010) <= Discription.size())
                     {
-                      Num << Discription.at(N);
+                      Num << Discription.at(N-3011);
                     }
                     else
                     {
@@ -453,57 +453,63 @@ void JournalDialog::FillMeasJour(QVector<S2::DataRec>* File)
     QVector<QVector<QVariant> > lsl;
     QByteArray OscInfo;
     char *mem;
-    int N = 0;
+    //int N = 0;
     int JourSize = 0; // размер считанного буфера с информацией
     int RecordSize = sizeof(MeasureStruct);
-    SystemWorkStruct System;
+    MeasureStruct Meas;
     size_t tmpt = static_cast<size_t>(RecordSize);
-    QStringList Discription = QStringList() << "Отсутствует сигнал напряжения фазы A"
-                                            << "Отсутствует сигнал напряжения фазы B"
-                                            << "Отсутствует сигнал напряжения фазы С"
-                                            << "Нет реактивного тока канала А (ток меньше 2мА)"
-                                            << "Нет реактивного тока канала B (ток меньше 2мА)"
-                                            << "Нет реактивного тока канала C (ток меньше 2мА)"
-                                            << "Не заданы начальные значения"
-                                            << "Низкое напряжение фазы A"
-                                            << "Низкое напряжение фазы B"
-                                            << "Низкое напряжение фазы C"
-                                            << "Сигнализация по приращению тангенса дельта ввода фазы А"
-                                            << "Сигнализация по приращению тангенса дельта ввода фазы B"
-                                            << "Сигнализация по приращению тангенса дельта ввода фазы C"
-                                            << "Авария по приращению тангенса дельта ввода фазы А"
-                                            << "Авария по приращению тангенса дельта ввода фазы B"
-                                            << "Авария по приращению тангенса дельта ввода фазы C"
-                                            << "Сигнализация по приращению C ввода фазы А"
-                                            << "Сигнализация по приращению C ввода фазы B"
-                                            << "Сигнализация по приращению C ввода фазы C"
-                                            << "Авария по приращению C ввода фазы А"
-                                            << "Авария по приращению C ввода фазы B"
-                                            << "Авария по приращению C ввода фазы C";
 
     memcpy(&JourSize, &(File->at(0).num_byte), 4);
 
         TableMeasModel->ClearModel();
-        TableMeasModel->addColumn(" № ");
-        TableMeasModel->addColumn("Описание события");
+        TableMeasModel->addColumn("Номер события");
         TableMeasModel->addColumn("Дата/Время");
-        TableMeasModel->addColumn("Тип события");
+        TableMeasModel->addColumn("Действующее значение напряжения фазы А, кВ");
+        TableMeasModel->addColumn("Действующее значение напряжения фазы B, кВ");
+        TableMeasModel->addColumn("Действующее значение напряжения фазы C, кВ");
+        TableMeasModel->addColumn("Действующее значение тока фазы А, мА");
+        TableMeasModel->addColumn("Действующее значение тока фазы B, мА");
+        TableMeasModel->addColumn("Действующее значение тока фазы C, мА");
+        TableMeasModel->addColumn("Напряжение нулевой последовательности");
+        TableMeasModel->addColumn("Напряжение прямой последовательности");
+        TableMeasModel->addColumn("Напряжение обратной последовательности");
+        TableMeasModel->addColumn("Ток нулевой последовательности");
+        TableMeasModel->addColumn("Ток прямой последовательности");
+        TableMeasModel->addColumn("Ток обратной последовательности");
+        TableMeasModel->addColumn("Емкость ввода фазы А, пФ");
+        TableMeasModel->addColumn("Емкость ввода фазы B, пФ");
+        TableMeasModel->addColumn("Емкость ввода фазы C, пФ");
+        TableMeasModel->addColumn("tg delta ввода фазы А, %");
+        TableMeasModel->addColumn("tg delta ввода фазы B, %");
+        TableMeasModel->addColumn("tg delta ввода фазы C, %");
+        TableMeasModel->addColumn("Изменение емкости ввода фазы А, % от C_init");
+        TableMeasModel->addColumn("Изменение емкости ввода фазы B, % от C_init");
+        TableMeasModel->addColumn("Изменение емкости ввода фазы C, % от C_init");
+        TableMeasModel->addColumn("Изменение tg delta ввода фазы А, %");
+        TableMeasModel->addColumn("Изменение tg delta ввода фазы B, %");
+        TableMeasModel->addColumn("Изменение tg delta ввода фазы C, %");
+        TableMeasModel->addColumn("Действующее значение 1-й гармоники тока небаланса, мА");
+        TableMeasModel->addColumn("Угол тока небаланса относительно тока ф.А, град");
+        TableMeasModel->addColumn("Температура кристалла микроконтроллера");
+        TableMeasModel->addColumn("Температура окружающей среды");
 
         QApplication::setOverrideCursor(Qt::WaitCursor);
-        QVector<QVariant> EventNum, Num, Time, Type;
-        int counter = 0;
+        QVector<QVariant> EventNum, Time, UeffA, UeffB, UeffC, IeffA, IeffB, IeffC, U0, U1, U2,
+                          I0, I1, I2, CbushA, CbushB, CbushC, Tg_dA, Tg_dB, Tg_dC, dCbushA, dCbushB,
+                          dCbushC, dTg_dA, dTg_dB, dTg_dC, Iunb, Phy_unb, Tmk, Tokr;
+        //int counter = 0;
 
         for(int i = 0; i < JourSize; i+= RecordSize)
         {
             mem = static_cast<char *>(File->at(0).thedata);
             mem +=i;
-            memcpy(&System, mem, tmpt);
+            memcpy(&Meas, mem, tmpt);
 
-            if(System.Time != 0xFFFFFFFFFFFFFFFF)
+            if(Meas.Time != 0xFFFFFFFF)
             {
-                if(LTime > System.Time)
+                if(LTime > Meas.Time)
                 {
-                  LTime = System.Time;
+                  LTime = Meas.Time;
                   SaveI = i;
                 }
             }
@@ -513,31 +519,44 @@ void JournalDialog::FillMeasJour(QVector<S2::DataRec>* File)
         {
             mem = static_cast<char *>(File->at(0).thedata);
             mem +=i;
-            memcpy(&System, mem, tmpt);
+            memcpy(&Meas, mem, tmpt);
 
-            if(System.Time != 0xFFFFFFFFFFFFFFFF)
+            if(Meas.Time != 0xFFFFFFFF)
             {
-                memcpy(&N, &System.EvNum, sizeof(System.EvNum));
-                N = N&0x00FFFFFF;
+                EventNum << Meas.NUM;
+                Time << TimeFunc::UnixTime64ToString(Meas.Time);
+                UeffA << Meas.Ueff[0];
+                UeffB << Meas.Ueff[1];
+                UeffC << Meas.Ueff[2];
+                IeffA << Meas.Ieff[0];
+                IeffB << Meas.Ieff[1];
+                IeffC << Meas.Ieff[2];
+                U0 << Meas.U0;
+                U1 << Meas.U1;
+                U2 << Meas.U2;
+                I0 << Meas.I0;
+                I1 << Meas.I1;
+                I2 << Meas.I2;
+                CbushA << Meas.Cbush[0];
+                CbushB << Meas.Cbush[1];
+                CbushC << Meas.Cbush[2];
+                Tg_dA << Meas.Tg_d[0];
+                Tg_dB << Meas.Tg_d[1];
+                Tg_dC << Meas.Tg_d[2];
+                dCbushA << Meas.dCbush[0];
+                dCbushB << Meas.dCbush[1];
+                dCbushC << Meas.dCbush[2];
+                dTg_dA << Meas.dTg_d[0];
+                dTg_dB << Meas.dTg_d[1];
+                dTg_dC << Meas.dTg_d[2];
+                Iunb << Meas.Iunb;
+                Phy_unb << Meas.Phy_unb;
+                Tmk << Meas.Tmk;
+                Tokr << Meas.Tamb;
 
-                if(N <= Discription.size())
-                {
-                  Num << Discription.at(N);
-                }
-                else
-                {
-                  Num << "Некорректный номер события";
-                }
 
-                Time << TimeFunc::UnixTime64ToString(System.Time);
-
-                if(System.EvType)
-                Type << "Пришло";
-                else
-                Type << "Ушло";
-
-                ++counter;
-                EventNum << counter;
+                //++counter;
+                //EventNum << counter;
             }
 
         }
@@ -548,40 +567,76 @@ void JournalDialog::FillMeasJour(QVector<S2::DataRec>* File)
             {
                 mem = static_cast<char *>(File->at(0).thedata);
                 mem +=i;
-                memcpy(&System, mem, tmpt);
+                memcpy(&Meas, mem, tmpt);
 
-                if(System.Time != 0xFFFFFFFFFFFFFFFF)
+                if(Meas.Time != 0xFFFFFFFF)
                 {
-                    memcpy(&N, &System.EvNum, sizeof(System.EvNum));
-                    N = N&0x00FFFFFF;
-
-                    if(N <= Discription.size())
-                    {
-                      Num << Discription.at(N);
-                    }
-                    else
-                    {
-                      Num << "Некорректный номер события";
-                    }
-
-                    Time << TimeFunc::UnixTime64ToString(System.Time);
-
-                    if(System.EvType)
-                    Type << "Пришло";
-                    else
-                    Type << "Ушло";
-
-                    ++counter;
-                    EventNum << counter;
+                    EventNum << Meas.NUM;
+                    Time << TimeFunc::UnixTime64ToString(Meas.Time);
+                    UeffA << Meas.Ueff[0];
+                    UeffB << Meas.Ueff[1];
+                    UeffC << Meas.Ueff[2];
+                    IeffA << Meas.Ieff[0];
+                    IeffB << Meas.Ieff[1];
+                    IeffC << Meas.Ieff[2];
+                    U0 << Meas.U0;
+                    U1 << Meas.U1;
+                    U2 << Meas.U2;
+                    I0 << Meas.I0;
+                    I1 << Meas.I1;
+                    I2 << Meas.I2;
+                    CbushA << Meas.Cbush[0];
+                    CbushB << Meas.Cbush[1];
+                    CbushC << Meas.Cbush[2];
+                    Tg_dA << Meas.Tg_d[0];
+                    Tg_dB << Meas.Tg_d[1];
+                    Tg_dC << Meas.Tg_d[2];
+                    dCbushA << Meas.dCbush[0];
+                    dCbushB << Meas.dCbush[1];
+                    dCbushC << Meas.dCbush[2];
+                    dTg_dA << Meas.dTg_d[0];
+                    dTg_dB << Meas.dTg_d[1];
+                    dTg_dC << Meas.dTg_d[2];
+                    Iunb << Meas.Iunb;
+                    Phy_unb << Meas.Phy_unb;
+                    Tmk << Meas.Tmk;
+                    Tokr << Meas.Tamb;
                 }
 
             }
         }
 
         lsl.append(EventNum);
-        lsl.append(Num);
         lsl.append(Time);
-        lsl.append(Type);
+        lsl.append(UeffA);
+        lsl.append(UeffB);
+        lsl.append(UeffC);
+        lsl.append(IeffA);
+        lsl.append(IeffB);
+        lsl.append(IeffC);
+        lsl.append(U0);
+        lsl.append(U1);
+        lsl.append(U2);
+        lsl.append(I0);
+        lsl.append(I1);
+        lsl.append(I2);
+        lsl.append(CbushA);
+        lsl.append(CbushB);
+        lsl.append(CbushC);
+        lsl.append(Tg_dA);
+        lsl.append(Tg_dB);
+        lsl.append(Tg_dC);
+        lsl.append(dCbushA);
+        lsl.append(dCbushB);
+        lsl.append(dCbushC);
+        lsl.append(dTg_dA);
+        lsl.append(dTg_dB);
+        lsl.append(dTg_dC);
+        lsl.append(Iunb);
+        lsl.append(Phy_unb);
+        lsl.append(Tmk);
+        lsl.append(Tokr);
+
         TableMeasModel->fillModel(lsl);
         UpdateSysModel();
         QApplication::restoreOverrideCursor();
