fe5e339af7e1159874a1592aa1e34aecde428472
atom <tomasiche@gmail.com>
2020-10-02 13:18:15 +0300

    [+] Implement eth scanner for subnet by ip and mask

--- dialogs/connectdialog.cpp
+++ dialogs/connectdialog.cpp
@@ -2,15 +2,23 @@
 
 #include "../gen/board.h"
 #include "../gen/error.h"
+#include "../gen/stdfunc.h"
 #include "../usb/eprotocom.h"
 #include "../widgets/wd_func.h"
 
+#include <QDebug>
+#include <QFuture>
+#include <QFutureWatcher>
+#include <QInputDialog>
 #include <QMessageBox>
+#include <QProgressDialog>
 #include <QSerialPortInfo>
 #include <QSettings>
 #include <QStandardItemModel>
-#include <QStringListModel>
 #include <QVBoxLayout>
+#include <QtConcurrent/QtConcurrent>
+#include <QtConcurrent/QtConcurrentMap>
+#include <QtNetwork/QHostAddress>
 
 ConnectDialog::ConnectDialog()
 {
@@ -58,8 +66,8 @@ void ConnectDialog::SetInterface()
         lyout->addWidget(table);
         QHBoxLayout *hlyout = new QHBoxLayout;
         hlyout->addStretch(10);
-        hlyout->addWidget(WDFunc::NewPB(dlg, "newethpb", "Добавить", this, SLOT(AddEth())));
-        hlyout->addWidget(WDFunc::NewPB(dlg, "scanethpb", "Сканировать", this, SLOT(ScanEth())));
+        hlyout->addWidget(WDFunc::NewPB2(dlg, "newethpb", "Добавить", this, &ConnectDialog::AddEth));
+        hlyout->addWidget(WDFunc::NewPB2(dlg, "scanethpb", "Сканировать", this, &ConnectDialog::ScanEth));
         hlyout->addStretch(10);
         lyout->addLayout(hlyout);
         connect(table, &QTableView::doubleClicked, this, qOverload<QModelIndex>(&ConnectDialog::SetEth));
@@ -126,7 +134,7 @@ void ConnectDialog::AddEth()
     ++count;
     lyout->addWidget(WDFunc::NewLBL(dlg, "Адрес БС:"), count, 0, 1, 1, Qt::AlignLeft);
     lyout->addWidget(WDFunc::NewSPB(dlg, "bsadrspb", 1, 255, 0), count++, 1, 1, 7);
-    lyout->addWidget(WDFunc::NewPB(dlg, "acceptpb", "Сохранить", this, SLOT(EthAccepted())), count, 0, 1, 4);
+    lyout->addWidget(WDFunc::NewPB2(dlg, "acceptpb", "Сохранить", this, &ConnectDialog::EthAccepted), count, 0, 1, 4);
     lyout->addWidget(WDFunc::NewPB(dlg, "cancelpb", "Отмена", dlg, SLOT(close())), count, 4, 1, 3);
     dlg->setLayout(lyout);
     dlg->exec();
@@ -218,6 +226,107 @@ void ConnectDialog::SetEth(QModelIndex index)
     st.iec104st.ip = mdl->data(mdl->index(row, 1)).toString();
     st.iec104st.baseadr = mdl->data(mdl->index(row, 2)).toUInt();
     emit Accepted(&st);
+}
+
+void ConnectDialog::handlePing()
+{
+    QFutureWatcher<quint32> *watcher = static_cast<QFutureWatcher<quint32> *>(sender());
+    Q_ASSERT(watcher);
+    if (watcher->result())
+    {
+        quint32 ip_addr = watcher->result();
+        m_hosts.append(ip_addr);
+        qDebug() << QHostAddress(m_hosts.last());
+    }
+    watcher->deleteLater();
+}
+
+void ConnectDialog::handlePingFinish()
+{
+    createPortTask();
+}
+
+void ConnectDialog::handlePortFinish()
+{
+    m_hosts.removeAll(0);
+    for (const auto &host : qAsConst(m_hosts))
+    {
+        qDebug() << QHostAddress(host);
+    }
+    QTableView *tv = this->findChild<QTableView *>();
+    QStandardItemModel *mdl = qobject_cast<QStandardItemModel *>(tv->model());
+    mdl->removeRows(0, mdl->rowCount());
+    for (const auto &host : qAsConst(m_hosts))
+    {
+        QList<QStandardItem *> row { new QStandardItem("AVM"), new QStandardItem(QHostAddress(host).toString()),
+            new QStandardItem("205") };
+        mdl->appendRow(row);
+    }
+}
+
+void ConnectDialog::createPingTask(quint32 ip)
+{
+    QFutureWatcher<quint32> *watcher = new QFutureWatcher<quint32>;
+
+    connect(watcher, &QFutureWatcher<quint32>::finished, this, &ConnectDialog::handlePing);
+    connect(watcher, &QFutureWatcher<quint32>::finished, [this]() {
+        m_progress->setValue(m_progress->value() + 1);
+        if (m_progress->value() == -1)
+            emit PingFinished();
+    });
+    QFuture<quint32> future = QtConcurrent::run(&StdFunc::ping, ip);
+    watcher->setFuture(future);
+}
+
+void ConnectDialog::createPortTask()
+{
+    QFutureWatcher<QList<quint32>> *watcher = new QFutureWatcher<QList<quint32>>;
+    quint16 port = 2404;
+
+    m_progress->setLabelText("Port scanning");
+    connect(watcher, &QFutureWatcher<QList<quint32>>::progressRangeChanged, m_progress, &QProgressDialog::setRange);
+    connect(watcher, &QFutureWatcher<QList<quint32>>::progressValueChanged, m_progress, &QProgressDialog::setValue);
+    QFuture<QList<quint32>> future;
+    future = QtConcurrent::mappedReduced(m_hosts,
+        std::bind(qOverload<quint32, quint16>(&StdFunc::checkPort), std::placeholders::_1, port),
+        &StdFunc::joinItem<quint32>);
+    watcher->setFuture(future);
+    future.waitForFinished();
+    m_hosts = future.result();
+    watcher->deleteLater();
+    handlePortFinish();
+}
+
+void ConnectDialog::ScanEth()
+{
+    /// Ping works only for windows
+    ///
+    auto *button = qobject_cast<QPushButton *>(sender());
+    Q_ASSERT(button);
+    bool ok;
+    QString text = QInputDialog::getText(
+        this, tr("Scanner"), tr("Ip address and mask:"), QLineEdit::Normal, "172.16.29.0/24", &ok);
+    QPair<QHostAddress, int> subnet = QHostAddress::parseSubnet(text);
+    if (!ok || text.isEmpty() || (subnet.second == -1))
+        return;
+    button->hide();
+    m_hosts.clear();
+
+    qDebug() << subnet.first << subnet.second;
+    quint32 addr_count = std::pow(2, 32 - subnet.second);
+    QDialog *dlg = this->findChild<QDialog *>("connectdlg");
+
+    m_progress = new QProgressDialog(dlg);
+    m_progress->setLabelText("Scanning ping");
+    m_progress->setMinimumDuration(0);
+    m_progress->setRange(0, addr_count - 1);
+
+    disconnect(this, &ConnectDialog::PingFinished, nullptr, nullptr);
+    connect(this, &ConnectDialog::PingFinished, &ConnectDialog::handlePingFinish);
+    for (quint32 i = 0; i < addr_count; ++i)
+    {
+        createPingTask(subnet.first.toIPv4Address() + i);
+    }
 }
 
 void ConnectDialog::AddRs()
