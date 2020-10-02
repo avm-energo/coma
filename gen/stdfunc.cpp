2604e4f49174dc2afa5fcfcffc573c6141c1881b
atom <tomasiche@gmail.com>
2020-10-02 13:16:32 +0300

    [+] Add ping function [+] Add checkPort function

--- gen/stdfunc.cpp
+++ gen/stdfunc.cpp
@@ -8,10 +8,15 @@
 #include <QElapsedTimer>
 #include <QFile>
 #include <QFileDialog>
+#include <QHostAddress>
+#include <QProcess>
 #include <QSettings>
 #include <QStandardPaths>
+#include <QTcpSocket>
+#include <QTextCodec>
 #include <QTextStream>
 #include <QThread>
+#include <QTimer>
 
 QString StdFunc::HomeDir = "";       // рабочий каталог программы
 QString StdFunc::SystemHomeDir = ""; // системный каталог программы
@@ -155,4 +160,79 @@ void StdFunc::Wait(int ms)
         QCoreApplication::processEvents(QEventLoop::AllEvents);
         QThread::msleep(MAINSLEEP);
     }
+}
+
+/*!
+     \brief Ping ip address, return ip address if host is alive or return 0 if host is dead
+
+Platform dependent ping function, ping ip address through cmdline utility, parse cmd output.
+If output contains TTL host is alive else host is dead.
+Returns ip address if host is alive; otherwise returns 0.
+    */
+quint32 StdFunc::ping(quint32 addr)
+{
+    QString exec = "ping";
+    QHostAddress host(addr);
+    auto *pingProcess = new QProcess();
+    QObject::connect(pingProcess, &QProcess::finished, &QObject::deleteLater);
+    QStringList params;
+
+    params << "-n"
+           << "1" << host.toString();
+    pingProcess->start(exec, params, QIODevice::ReadOnly);
+    if (pingProcess->waitForFinished(100))
+    {
+        QTextCodec *codec = QTextCodec::codecForMib(2086);
+        QString p_stdout = codec->toUnicode(pingProcess->readAllStandardOutput());
+        QString p_stderr = codec->toUnicode(pingProcess->readAllStandardError());
+        QStringList list = p_stderr.isEmpty() ? p_stdout.split("\r\n", Qt::SkipEmptyParts)
+                                              : p_stderr.split("\r\n", Qt::SkipEmptyParts);
+
+        if (std::any_of(list.constBegin(), list.constEnd(), [](const QString &i) { return i.contains("TTL"); }))
+        {
+            pingProcess->deleteLater();
+            return addr;
+        }
+    }
+
+    pingProcess->kill();
+    return 0;
+}
+
+/*! \brief Check port port for ip4Addr ip address
+
+    \param ip address of host, ip4Addr, port for checking, port
+    \return Ip address of host if port is open otherwise return 0;
+*/
+quint32 StdFunc::checkPort(quint32 ip4Addr, quint16 port)
+{
+    QHostAddress host(ip4Addr);
+    QTcpSocket *sock = new QTcpSocket;
+    QTimer *timer = new QTimer;
+    timer->setInterval(1000);
+    QEventLoop *loop = new QEventLoop;
+
+    QObject::connect(sock, &QAbstractSocket::connected, [&]() {
+        qDebug() << "Port opened";
+        timer->stop();
+        timer->deleteLater();
+        sock->disconnectFromHost();
+        sock->deleteLater();
+        loop->quit();
+        loop->deleteLater();
+    });
+    QObject::connect(timer, &QTimer::timeout, [&]() {
+        qDebug() << "Port closed";
+        sock->disconnectFromHost();
+        sock->deleteLater();
+        loop->quit();
+        loop->deleteLater();
+        timer->deleteLater();
+        ip4Addr = 0;
+    });
+    timer->start();
+    qDebug() << "Timer started";
+    sock->connectToHost(host, port);
+    loop->exec(QEventLoop::ExcludeUserInputEvents);
+    return ip4Addr;
 }
