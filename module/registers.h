1119a7b1764d888a33ed77dc07221bdacb2c6dc3
atom <tomasiche@gmail.com>
2020-11-19 13:49:10 +0300

    [+] Move registers to their own file

---
+++ module/registers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//#define INITREG 4000
+//#define INITREGCOUNT 11 // 11 floats results in 22 bytes
+#define TIMEREG 4600
+#define TIMEREGCOUNT 1
+#define SETINITREG 900
+#define CLEARREG 905
+#define BSIREG 1
+#define BSIHTHREG 15
+#define BSIENDREG 15 // конечный регистр BSI в 104 и modbus
