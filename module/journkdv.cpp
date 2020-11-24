040ad5760cccf1405e68457a94d5e1c9ac9eb3aa
atom <tomasiche@gmail.com>
2020-11-24 18:06:45 +0300

    [+] Move out module dependent info from classes to modules.h (KDV)

--- module/journkdv.cpp
+++ module/journkdv.cpp
@@ -1,6 +1,6 @@
 #include "journkdv.h"
 
-#include "../gen/timefunc.h"
+#include "modules.h"
 
 JournKDV::JournKDV(QObject *parent) : Journals(parent)
 {
@@ -8,73 +8,18 @@ JournKDV::JournKDV(QObject *parent) : Journals(parent)
 
 void JournKDV::setWorkJourDescription()
 {
-    m_workJourDescription = QStringList() << "Отсутствует сигнал напряжения фазы A"
-                                          << "Отсутствует сигнал напряжения фазы B"
-                                          << "Отсутствует сигнал напряжения фазы С"
-                                          << "Нет реактивного тока канала А (ток меньше 2мА)"
-                                          << "Нет реактивного тока канала B (ток меньше 2мА)"
-                                          << "Нет реактивного тока канала C (ток меньше 2мА)"
-                                          << "Не заданы начальные значения"
-                                          << "Низкое напряжение фазы A"
-                                          << "Низкое напряжение фазы B"
-                                          << "Низкое напряжение фазы C"
-                                          << "Сигнализация по приращению тангенса дельта ввода фазы А"
-                                          << "Сигнализация по приращению тангенса дельта ввода фазы B"
-                                          << "Сигнализация по приращению тангенса дельта ввода фазы C"
-                                          << "Авария по приращению тангенса дельта ввода фазы А"
-                                          << "Авария по приращению тангенса дельта ввода фазы B"
-                                          << "Авария по приращению тангенса дельта ввода фазы C"
-                                          << "Сигнализация по приращению C ввода фазы А"
-                                          << "Сигнализация по приращению C ввода фазы B"
-                                          << "Сигнализация по приращению C ввода фазы C"
-                                          << "Авария по приращению C ввода фазы А"
-                                          << "Авария по приращению C ввода фазы B"
-                                          << "Авария по приращению C ввода фазы C"
-                                          << "Не заданы паспортные значения"
-                                          << "Сигнализация по изменению небаланса тока"
-                                          << "Авария по изменению небаланса тока";
+    m_workJourDescription = AVM_KDV::workJourDescription;
 }
 
 void JournKDV::setMeasJourHeaders()
 {
-    m_measJourHeaders = QStringList() << " № "
-                                      << "Дата/Время UTC"
-                                      << "Ueff фA"
-                                      << "Ueff фB"
-                                      << "Ueff фC"
-                                      << "Ieff фA"
-                                      << "Ieff фB"
-                                      << "Ieff фC"
-                                      << "Freq"
-                                      << "U1"
-                                      << "U2"
-                                      << "I1"
-                                      << "I2"
-                                      << "Pf_sum"
-                                      << "Qf_sum"
-                                      << "Sf_sum"
-                                      << "CosPhi"
-                                      << "Tmk"
-                                      << "Tamb"
-                                      << "Twin"
-                                      << "VibrSCZ_V"
-                                      << "VibrSCZ_V"
-                                      << "VibrSCZ_V"
-                                      << "VibrSCZ_V"
-                                      << "VibrSCZ_V"
-                                      << "VibrSCZ_V"
-                                      << "VibrSCZ_D"
-                                      << "VibrSCZ_D"
-                                      << "VibrSCZ_D"
-                                      << "VibrSCZ_D"
-                                      << "VibrSCZ_D"
-                                      << "VibrSCZ_D";
+    m_measJourHeaders = AVM_KDV::measJourHeaders;
 }
 
 void JournKDV::setMeasRecord(char *file, QVector<QVariant> &outvl)
 {
-    MeasureStruct KDVdata;
-    memcpy(&KDVdata, file, sizeof(MeasureStruct));
+    AVM_KDV::MeasureStruct KDVdata;
+    memcpy(&KDVdata, file, sizeof(AVM_KDV::MeasureStruct));
     //    if (KDVdata.Time != 0xFFFFFFFF)
     //        outvl = { KDVdata.NUM, TimeFunc::UnixTime32ToInvString(KDVdata.Time), KDVdata.Ueff[0], KDVdata.Ueff[1],
     //            KDVdata.Ueff[2], KDVdata.Ieff[0], KDVdata.Ieff[1], KDVdata.Ieff[2], KDVdata.Frequency, KDVdata.U0,
@@ -86,10 +31,10 @@ void JournKDV::setMeasRecord(char *file, QVector<QVariant> &outvl)
 
 int JournKDV::measureSize()
 {
-    return sizeof(MeasureStruct);
+    return sizeof(AVM_KDV::MeasureStruct);
 }
 
 int JournKDV::workJournalID()
 {
-    return KDVWORKJOURNID;
+    return AVM_KDV::workJourId;
 }
