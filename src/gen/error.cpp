af412830fa34151ee3b8b66c5cc47e0ebf85345e
atom <tomasiche@gmail.com>
2021-02-09 16:55:01 +0300

    [*] Fix error time in error journal

--- src/gen/error.cpp
+++ src/gen/error.cpp
@@ -7,8 +7,7 @@
 #include <QTextStream>
 QStringList &operator<<(QStringList &l, const ErrorMsg &obj)
 {
-    l << QDateTime::currentDateTime().toString("dd-MM-yyyy hh:mm:ss") << obj.file << QString::number(obj.line, 10)
-      << obj.msg;
+    l << obj.DateTime << obj.file << QString::number(obj.line, 10) << obj.msg;
     return l;
 }
 
