d5fcf8fafa2b414434d5f7d1be6f95e48fe3b1a4
atom <tomasiche@gmail.com>
2021-03-03 13:51:30 +0300

    Revert commit ceead1c23ef4b7f13cd8adbb79135b8269feb277
    Should be tested with all commands before implementation

--- src/interfaces/protocomthread.cpp
+++ src/interfaces/protocomthread.cpp
@@ -482,18 +482,10 @@ void ProtocomThread::parseResponse(QByteArray ba)
 
         // Потому что на эту команду модуль не отдает пустой ответ
         if (isOneSegment(size) || (cmd == Proto::ReadBlkStartInfo))
-        {
             handle(Proto::Commands(cmd));
-            if (progress != NULL)
-            {
-                handleMaxProgress(progress = +Proto::Limits::MaxSegmenthLength);
-                progress = NULL;
-            }
-        }
         else
         {
-            progress += Proto::Limits::MaxSegmenthLength;
-            handleProgress(progress);
+
             auto tba = prepareOk(false, cmd);
             Q_ASSERT(tba.size() == 4);
             emit writeDataAttempt(tba);
