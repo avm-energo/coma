eef39d08cb6c91f4220091cfdebdf19f662d2707
atom <tomasiche@gmail.com>
2021-04-06 17:10:53 +0300

    [+] GitHelper

---
+++ src/comaversion/comaversion.cpp
@@ -0,0 +1,30 @@
+#include "comaversion.h"
+
+#include "gversion.h"
+
+QString GitVersion::getGitHash()
+{
+    const QString buffer(gitHash);
+    return buffer;
+}
+
+unsigned long long GitVersion::getGitCounter()
+{
+    return gitCommitCounter;
+}
+
+QString GitVersion::getConfigVersion()
+{
+    QString str;
+    auto counter = gitCommitCounter;
+    while (counter != 0)
+    {
+        str.prepend('.');
+        str.prepend(QString::number(counter % 10));
+
+        counter /= 10;
+    }
+    if (str.back() == '.')
+        str.push_back('0');
+    return str;
+}
