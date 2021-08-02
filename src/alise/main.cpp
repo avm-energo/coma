9d592df9a9fa5ba38ffe57826d3ca861ffdfd903
user <tomasiche@gmail.com>
2021-08-02 11:32:35 +0300

    [+] Add --version

--- src/alise/main.cpp
+++ src/alise/main.cpp
@@ -1,15 +1,31 @@
 
+#include "../comaversion/comaversion.h"
 #include "../gen/logger.h"
 #include "controller.h"
 #include "stmbroker.h"
 
 #include <QCoreApplication>
+#include <config.h>
 #include <iostream>
 int main(int argc, char *argv[])
 {
     std::cout << "Started " << std::endl;
 
+    GitVersion gitVersion;
     QCoreApplication a(argc, argv);
+    a.setApplicationVersion(QString(COMAVERSION) + "-" + gitVersion.getGitHash());
+
+    QCommandLineParser parser;
+
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    if (QCoreApplication::arguments().size() > 1)
+    {
+        parser.process(QCoreApplication::arguments());
+        return 0;
+    }
+
     Logging::writeStart();
     qInstallMessageHandler(Logging::messageHandler);
     Controller controller;
