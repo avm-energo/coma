312216397b11890a5dea7e77597e0bd647d69510
atom <tomasiche@gmail.com>
2021-10-13 11:30:59 +0300

    [*] QVERIFY -> QCOMPARE

--- src/tests/xmlParser/testmodule.cpp
+++ src/tests/xmlParser/testmodule.cpp
@@ -13,11 +13,11 @@ void TestModule::checkA284()
     QString str("KIV");
     QVERIFY(module.loadSettings(str, 0x84, 0xA2));
     auto settings = module.settings();
-    QVERIFY(settings->alarms.size() == 2);
-    QVERIFY(settings->configSettings.size() == 48);
-    QVERIFY(settings->highlightCrit.size() == 7);
-    QVERIFY(settings->highlightWarn.size() == 30);
-    QVERIFY(settings->journals.size() == 2);
+    QCOMPARE(settings->alarms.size(), 2);
+    QCOMPARE(settings->configSettings.size(), 48);
+    QCOMPARE(settings->highlightCrit.size(), 7);
+    QCOMPARE(settings->highlightWarn.size(), 30);
+    QCOMPARE(settings->journals.size(), 2);
     QVERIFY(!settings->ifaceSettings.settings.isValid());
 }
 
@@ -27,15 +27,15 @@ void TestModule::checkA284USB()
     QVERIFY(module.loadSettings(str, 0x84, 0xA2, Board::InterfaceType::USB));
     auto settings = module.settings();
     QVERIFY(settings->ifaceSettings.settings.isValid());
-    QVERIFY(settings->alarms.size() == 2);
-    QVERIFY(settings->configSettings.size() == 48);
-    QVERIFY(settings->highlightCrit.size() == 7);
-    QVERIFY(settings->highlightWarn.size() == 30);
-    QVERIFY(settings->journals.size() == 2);
+    QCOMPARE(settings->alarms.size(), 2);
+    QCOMPARE(settings->configSettings.size(), 48);
+    QCOMPARE(settings->highlightCrit.size(), 7);
+    QCOMPARE(settings->highlightWarn.size(), 30);
+    QCOMPARE(settings->journals.size(), 2);
     QVERIFY(settings->ifaceSettings.settings.canConvert<InterfaceInfo<Proto::ProtocomGroup>>());
     const auto &st = settings->ifaceSettings.settings.value<InterfaceInfo<Proto::ProtocomGroup>>();
-    QVERIFY(st.dictionary().size() == 8);
-    QVERIFY(st.groups().size() == 8);
+    QCOMPARE(st.dictionary().size(), 8);
+    QCOMPARE(st.groups().size(), 8);
 }
 
 void TestModule::checkA284Eth()
@@ -44,11 +44,11 @@ void TestModule::checkA284Eth()
     QVERIFY(module.loadSettings(str, 0x84, 0xA2, Board::InterfaceType::Ethernet));
     auto settings = module.settings();
     QVERIFY(settings->ifaceSettings.settings.isValid());
-    QVERIFY(settings->alarms.size() == 2);
-    QVERIFY(settings->configSettings.size() == 48);
-    QVERIFY(settings->highlightCrit.size() == 7);
-    QVERIFY(settings->highlightWarn.size() == 30);
-    QVERIFY(settings->journals.size() == 2);
+    QCOMPARE(settings->alarms.size(), 2);
+    QCOMPARE(settings->configSettings.size(), 48);
+    QCOMPARE(settings->highlightCrit.size(), 7);
+    QCOMPARE(settings->highlightWarn.size(), 30);
+    QCOMPARE(settings->journals.size(), 2);
 }
 
 void TestModule::checkA284Modbus()
@@ -57,17 +57,17 @@ void TestModule::checkA284Modbus()
     QVERIFY(module.loadSettings(str, 0x84, 0xA2, Board::InterfaceType::RS485));
     auto settings = module.settings();
     QVERIFY(settings->ifaceSettings.settings.isValid());
-    QVERIFY(settings->alarms.size() == 2);
-    QVERIFY(settings->configSettings.size() == 48);
-    QVERIFY(settings->highlightCrit.size() == 7);
-    QVERIFY(settings->highlightWarn.size() == 30);
-    QVERIFY(settings->journals.size() == 2);
+    QCOMPARE(settings->alarms.size(), 2);
+    QCOMPARE(settings->configSettings.size(), 48);
+    QCOMPARE(settings->highlightCrit.size(), 7);
+    QCOMPARE(settings->highlightWarn.size(), 30);
+    QCOMPARE(settings->journals.size(), 2);
     QVERIFY(settings->ifaceSettings.settings.canConvert<InterfaceInfo<CommandsMBS::ModbusGroup>>());
     const auto &st = settings->ifaceSettings.settings.value<InterfaceInfo<CommandsMBS::ModbusGroup>>();
-    QVERIFY(st.dictionary().size() == 15);
-    QVERIFY(st.groups().size() == 15);
-    QVERIFY(st.regs().size() == 2);
-    QVERIFY(st.dictionaryRegs().size() == 2);
+    QCOMPARE(st.dictionary().size(), 15);
+    QCOMPARE(st.groups().size(), 15);
+    QCOMPARE(st.regs().size(), 2);
+    QCOMPARE(st.dictionaryRegs().size(), 2);
 }
 
 void TestModule::check8084()
@@ -75,11 +75,11 @@ void TestModule::check8084()
     QString str("АВ-ТУК-8084");
     QVERIFY(module.loadSettings(str, 0x84, 0x80));
     auto settings = module.settings();
-    QVERIFY(settings->alarms.size() == 2);
-    QVERIFY(settings->configSettings.size() == 39);
-    QVERIFY(settings->highlightCrit.size() == 0);
-    QVERIFY(settings->highlightWarn.size() == 0);
-    QVERIFY(settings->journals.size() == 2);
+    QCOMPARE(settings->alarms.size(), 2);
+    QCOMPARE(settings->configSettings.size(), 39);
+    QCOMPARE(settings->highlightCrit.size(), 0);
+    QCOMPARE(settings->highlightWarn.size(), 0);
+    QCOMPARE(settings->journals.size(), 2);
     QVERIFY(!settings->ifaceSettings.settings.isValid());
 }
 
@@ -88,17 +88,17 @@ void TestModule::check8084USB()
     QString str("АВ-ТУК-8084");
     QVERIFY(module.loadSettings(str, 0x84, 0x80, Board::InterfaceType::USB));
     auto settings = module.settings();
-    QVERIFY(settings->alarms.size() == 2);
-    QVERIFY(settings->configSettings.size() == 39);
-    QVERIFY(settings->highlightCrit.size() == 0);
-    QVERIFY(settings->highlightWarn.size() == 0);
-    QVERIFY(settings->journals.size() == 2);
+    QCOMPARE(settings->alarms.size(), 2);
+    QCOMPARE(settings->configSettings.size(), 39);
+    QCOMPARE(settings->highlightCrit.size(), 0);
+    QCOMPARE(settings->highlightWarn.size(), 0);
+    QCOMPARE(settings->journals.size(), 2);
     QVERIFY(settings->ifaceSettings.settings.canConvert<InterfaceInfo<Proto::ProtocomGroup>>());
     const auto &st = settings->ifaceSettings.settings.value<InterfaceInfo<Proto::ProtocomGroup>>();
-    QVERIFY(st.dictionary().size() == 8);
-    QVERIFY(st.groups().size() == 8);
-    QVERIFY(st.regs().size() == 0);
-    QVERIFY(st.dictionaryRegs().size() == 0);
+    QCOMPARE(st.dictionary().size(), 8);
+    QCOMPARE(st.groups().size(), 8);
+    QCOMPARE(st.regs().size(), 0);
+    QCOMPARE(st.dictionaryRegs().size(), 0);
 }
 
 void TestModule::check8085()
@@ -150,8 +150,8 @@ void TestModule::check3533USB()
     QVERIFY(settings->ifaceSettings.settings.isValid());
     QVERIFY(settings->ifaceSettings.settings.canConvert<InterfaceInfo<Proto::ProtocomGroup>>());
     const auto &st = settings->ifaceSettings.settings.value<InterfaceInfo<Proto::ProtocomGroup>>();
-    QVERIFY(st.dictionary().size() == 2);
-    QVERIFY(st.groups().size() == 2);
+    QCOMPARE(st.dictionary().size(), 2);
+    QCOMPARE(st.groups().size(), 2);
 }
 
 QTEST_GUILESS_MAIN(TestModule)
