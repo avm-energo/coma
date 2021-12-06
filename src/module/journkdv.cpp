e1e23c3173fce02edd13c6748478305eb94a29c9
atom <tomasiche@gmail.com>
2021-12-06 16:49:27 +0300

    [+] Enable meas jour for KDV

--- src/module/journkdv.cpp
+++ src/module/journkdv.cpp
@@ -13,15 +13,16 @@ QVector<QVariant> JournKDV::createMeasRecord(const char *file)
 {
     AVM_KDV::MeasureStruct KDVdata;
     memcpy(&KDVdata, file, sizeof(AVM_KDV::MeasureStruct));
-    //    if (KDVdata.Time != 0xFFFFFFFF)
-    //            outvl = { KDVdata.NUM, TimeFunc::UnixTime32ToInvString(KDVdata.Time), KDVdata.Ueff[0],
-    //            KDVdata.Ueff[1],
-    //                KDVdata.Ueff[2], KDVdata.Ieff[0], KDVdata.Ieff[1], KDVdata.Ieff[2], KDVdata.Frequency, KDVdata.U0,
-    //                KDVdata.U1, KDVdata.U2, KDVdata.I0, KDVdata.I1, KDVdata.I2, KDVdata.Cbush[0], KDVdata.Cbush[1],
-    //                KDVdata.Cbush[2], KDVdata.Tg_d[0], KDVdata.Tg_d[1], KDVdata.Tg_d[2], KDVdata.dCbush[0],
-    //                KDVdata.dCbush[1], KDVdata.dCbush[2], KDVdata.dTg_d[0], KDVdata.dTg_d[1], KDVdata.dTg_d[2],
-    //                KDVdata.Iunb, KDVdata.Phy_unb, KDVdata.Tmk, KDVdata.Tamb };
-    return QVector<QVariant>();
+    if (KDVdata.Time == 0xFFFFFFFF)
+        return QVector<QVariant>();
+
+    QVector<QVariant> outvl = { KDVdata.NUM, TimeFunc::UnixTime32ToInvString(KDVdata.Time, m_timezone), KDVdata.Ueff[0],
+        KDVdata.Ueff[1], KDVdata.Ueff[2], KDVdata.Ieff[0], KDVdata.Ieff[1], KDVdata.Ieff[2], KDVdata.Frequency,
+        KDVdata.U1, KDVdata.U2, KDVdata.I1, KDVdata.I2, KDVdata.Pf_sum, KDVdata.Qf_sum, KDVdata.Sf_sum, KDVdata.CosPhi,
+        KDVdata.Tmk, KDVdata.Tamb, KDVdata.Twin, KDVdata.VibrSCZ_V[0], KDVdata.VibrSCZ_V[1], KDVdata.VibrSCZ_V[2],
+        KDVdata.VibrSCZ_V[3], KDVdata.VibrSCZ_V[4], KDVdata.VibrSCZ_V[5], KDVdata.VibrSCZ_D[0], KDVdata.VibrSCZ_D[1],
+        KDVdata.VibrSCZ_D[2], KDVdata.VibrSCZ_D[3], KDVdata.VibrSCZ_D[4], KDVdata.VibrSCZ_D[5] };
+    return outvl;
 }
 
 QVector<QVector<QVariant>> JournKDV::createMeas(const QByteArray &array)
@@ -43,17 +44,17 @@ QVector<QVector<QVariant>> JournKDV::createMeas(const QByteArray &array)
     std::sort(data.begin(), data.end(),
         [](const MeasureStruct &lhs, const MeasureStruct &rhs) { return lhs.Time > rhs.Time; });
     QVector<QVector<QVariant>> ValueLists;
-    // NOTE Fix it to enable jours
-    //    std::transform(data.cbegin(), data.cend(), std::back_inserter(ValueLists), [this](const MeasureStruct
-    //    &kdvdata) {
-    //        QVector<QVariant> outvl { kdvdata.NUM, TimeFunc::UnixTime32ToInvString(kdvdata.Time, m_timezone),
-    //            kdvdata.Ueff[0], kdvdata.Ueff[1], kdvdata.Ueff[2], kdvdata.Ieff[0], kdvdata.Ieff[1], kdvdata.Ieff[2],
-    //            kdvdata.Frequency, kdvdata.U0, kdvdata.U1, kdvdata.U2, kdvdata.I0, kdvdata.I1, kdvdata.I2,
-    //            kdvdata.Cbush[0], kdvdata.Cbush[1], kdvdata.Cbush[2], kdvdata.Tg_d[0], kdvdata.Tg_d[1],
-    //            kdvdata.Tg_d[2], kdvdata.dCbush[0], kdvdata.dCbush[1], kdvdata.dCbush[2], kdvdata.dTg_d[0],
-    //            kdvdata.dTg_d[1], kdvdata.dTg_d[2], kdvdata.Iunb, kdvdata.Phy_unb, kdvdata.Tmk, kdvdata.Tamb };
-    //        return outvl;
-    //    });
+
+    std::transform(data.cbegin(), data.cend(), std::back_inserter(ValueLists), [this](const MeasureStruct &kdvdata) {
+        QVector<QVariant> outvl { kdvdata.NUM, TimeFunc::UnixTime32ToInvString(kdvdata.Time, m_timezone),
+            kdvdata.Ueff[0], kdvdata.Ueff[1], kdvdata.Ueff[2], kdvdata.Ieff[0], kdvdata.Ieff[1], kdvdata.Ieff[2],
+            kdvdata.Frequency, kdvdata.U1, kdvdata.U2, kdvdata.I1, kdvdata.I2, kdvdata.Pf_sum, kdvdata.Qf_sum,
+            kdvdata.Sf_sum, kdvdata.CosPhi, kdvdata.Tmk, kdvdata.Tamb, kdvdata.Twin, kdvdata.VibrSCZ_V[0],
+            kdvdata.VibrSCZ_V[1], kdvdata.VibrSCZ_V[2], kdvdata.VibrSCZ_V[3], kdvdata.VibrSCZ_V[4],
+            kdvdata.VibrSCZ_V[5], kdvdata.VibrSCZ_D[0], kdvdata.VibrSCZ_D[1], kdvdata.VibrSCZ_D[2],
+            kdvdata.VibrSCZ_D[3], kdvdata.VibrSCZ_D[4], kdvdata.VibrSCZ_D[5] };
+        return outvl;
+    });
     return ValueLists;
 }
 
