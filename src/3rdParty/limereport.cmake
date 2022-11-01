1a87c59377e370aa954c77dd1f211c5e00ee09a0
vladd12 <svm@asu-vei.ru>
2022-11-01 11:39:01 +0300

    [*] Alise build try

--- src/3rdParty/limereport.cmake
+++ src/3rdParty/limereport.cmake
@@ -1,15 +1,13 @@
+cmake_minimum_required(VERSION 3.15)
 include(FetchContent)
-
-#set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
 
 FetchContent_Declare(
   limereport-qt5
-  GIT_REPOSITORY https://github.com/Jihadist/LimeReport.git
-  GIT_TAG        e6ead26d31385cc62e969339623ce9d45717b0f8
+  GIT_REPOSITORY https://github.com/fralx/LimeReport.git
+  GIT_TAG        b1c67651c8d90ee822893990574eb112ec76d575
 )
 # If we want compile lib in static
 #set(LIMEREPORT_STATIC ON)
 FetchContent_MakeAvailable(limereport-qt5)
 
 add_library(limereport ALIAS limereport-qt5)
-#target_include_directories(limereport-qt5 INTERFACE ${limereport_SOURCE_DIR}/include)
