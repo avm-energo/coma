ccd0bb3ff786fe76ce6994bcfad571c1b6d66de1
Anton <vao@asu-vei.ru>
2022-11-09 14:04:03 +0300

    [^] alise bump [*] gen is now being built from avm-energo repo

--- src/3rdParty/gen.cmake
+++ src/3rdParty/gen.cmake
@@ -3,8 +3,8 @@ include(FetchContent)
 
 FetchContent_Declare(
   gen
-  GIT_REPOSITORY https://github.com/vladd12/gen.git
-  GIT_TAG        71e8c041de54e358ae849f4ef1b90bcdf226d5dc
+  GIT_REPOSITORY ssh://git@git.avmenergo.ru:5507/avm-energo/gen.git
+  GIT_TAG        origin/main
 )
 set(GEN_STATIC ON)
 FetchContent_MakeAvailable(gen)
