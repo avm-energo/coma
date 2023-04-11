98c3ca20752399630ee614ca2007c5f9101f57a3
vladd12 <svm@asu-vei.ru>
2023-04-11 13:33:44 +0300

    [*] Renaming in JournalDialog class

--- src/dialogs/journalsdialog.h
+++ src/dialogs/journalsdialog.h
@@ -19,24 +19,25 @@ public:
     ~JournalDialog();
 
 signals:
-    void StartSaveJour(DataTypes::FilesEnum, QString);
+    void startSaveJour(DataTypes::FilesEnum, QString);
 
 private:
-    void SetupUI();
-    QWidget *JourTab(DataTypes::FilesEnum jourtype);
-    int GetJourNum(const QString &objname);
+    void setupUI();
+    QWidget *jourTab(DataTypes::FilesEnum jourtype);
+    int getJourNum(const QString &objname);
+    void createProgressDialog();
 
 private slots:
-    void JourFileChoosed(QString &file);
-    void EraseJour();
-    void SaveJour(DataTypes::FilesEnum jourType);
-    void Done(QString msg);
-    void Error(QString msg);
+    void jourFileChoosed(QString &file);
+    void eraseJour();
+    void saveJour(DataTypes::FilesEnum jourType);
+    void done(QString msg);
+    void error(QString msg);
 
 private:
     QString JourFile;
     UniquePointer<Journals> m_jour;
-    QSortFilterProxyModel *ProxyWorkModel, *ProxySysModel, *ProxyMeasModel;
+    QSortFilterProxyModel *proxyWorkModel, *proxySysModel, *proxyMeasModel;
     QProgressDialog *progress;
     QMap<DataTypes::FilesEnum, QTableView *> views;
 };
