a7d5f4cb3a14067c47c2602e2fb2a42400eae9fd
vladd12 <svm@asu-vei.ru>
2023-05-12 10:24:50 +0300

    [*] CI build fix: limereport unfortunately is not a dependency now

--- cmake/WinDeployQt.cmake
+++ cmake/WinDeployQt.cmake
@@ -12,8 +12,12 @@ function(win_deploy_qt PROJECT_NAME QT_VERSION_MAJOR QT_BIN_DIR)
       CODE [[
         execute_process(COMMAND ${QT_BIN_DIR}/windeployqt.exe --no-quick-import --no-system-d3d-compiler
             --no-virtualkeyboard --no-compiler-runtime --no-webkit2 --no-angle --no-opengl-sw
+            ${CMAKE_INSTALL_PREFIX}/${INSTALL_PROJECT_PATH}/gen.dll
+            ${CMAKE_INSTALL_PREFIX}/${INSTALL_PROJECT_PATH}/qcustomplot.dll
             ${CMAKE_INSTALL_PREFIX}/${INSTALL_PROJECT_PATH}/${PROJECT_NAME}.exe
-            ${CMAKE_INSTALL_PREFIX}/${INSTALL_PROJECT_PATH}/limereport-qt${QT_VERSION_MAJOR}.dll
-          WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/${INSTALL_PROJECT_PATH})
+#            ${CMAKE_INSTALL_PREFIX}/${INSTALL_PROJECT_PATH}/limereport-qt${QT_VERSION_MAJOR}.dll
+          WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/${INSTALL_PROJECT_PATH}
+          OUTPUT_VARIABLE OUTPUT)
+        message(STATUS "Copy status: ${OUTPUT}")
       ]])
 endfunction()
