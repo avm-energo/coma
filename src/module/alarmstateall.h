688a97454209ca2d4c01f0cc103705b790e409e7
vladd12 <vladd12z32@gmail.com>
2023-09-11 18:52:40 +0300

    Code style small reworking:
    [*] Move documenting commentaries to header files
    [*] Using STL containers instead Qt version
    [*] Using hungarian notation for class members
    [*] Small changes

--- src/module/alarmstateall.h
+++ src/module/alarmstateall.h
@@ -10,13 +10,16 @@ class AlarmStateAll : public BaseAlarm
 {
     Q_OBJECT
 private:
-    std::bitset<128> mAlarmFlags; // '1' equals alarm
+    std::bitset<128> m_alarmFlags; // '1' equals alarm
 
 public:
     explicit AlarmStateAll(QWidget *parent = nullptr);
+    /// \brief Setup UI: creating text labels and indicators (pixmaps) for alarms displaying.
     virtual void setupUI(const QStringList &events) override;
+    /// \brief Request for updating a BSI data.
     virtual void reqUpdate() override;
 
 public slots:
+    /// \brief The slot called when a device health changed for updating alarms.
     void update(quint32 health);
 };
--- src/module/configstorage.h
+++ src/module/configstorage.h
@@ -9,28 +9,37 @@
 #include <QObject>
 #include <gen/singleton.h>
 
+/// \brief Class for storaging XML settings for a selected device in ConnectionDialog.
 class ConfigStorage : public QObject, public Singleton<ConfigStorage>
 {
     Q_OBJECT
 private:
-    std::unique_ptr<ModuleSettings> mSettings;
+    ModuleSettings m_settings;
 
 public:
     explicit ConfigStorage(token, QObject *parent = nullptr);
+    /// \brief Constant getter for module settings.
     const ModuleSettings &getModuleSettings() const;
+    /// \brief Cleaning module settings.
     void clearModuleSettings();
 
 public slots:
-    // Module data slots
+    /// \brief Slot for saving device's signal records.
     void signalDataReceive(const quint32 id, const quint32 addr, //
         const quint16 count, const ModuleTypes::SignalType sigType);
+    /// \brief Slot for saving check's tab records.
     void tabDataReceive(const quint32 id, const QString &name);
+    /// \brief Slot for saving check's section records.
     void sectionDataReceive(const ModuleTypes::SGMap &sgmap, const QString &secHead);
+    /// \brief Slot for saving device's alarm records.
     void alarmDataReceive(const Modules::AlarmType aType, const quint32 addr, //
         const QString &desc, const QList<quint32> &highlights);
+    /// \brief Slot for saving module a work journal's record.
     void workJourDataReceive(const quint32 id, const QString &desc);
+    /// \brief Slot for saving module a measurement journal's record.
     void measJourDataReceive(const quint32 index, const QString &header, //
         const ModuleTypes::BinaryType type, bool visib);
+    /// \brief Slot for saving module's protocol groups
     void protocolDescriptionReceived(const parseXChangeStruct &str);
 };
 
