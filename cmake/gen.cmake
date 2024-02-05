c46b0cd9d80b771fe7168b840a2bb0cfc37f47cf
vladd12 <vladd12z32@gmail.com>
2024-02-05 15:32:07 +0300

    [^] version update
    [*] Fix gen commit

--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   gen
   GIT_REPOSITORY https://github.com/avm-energo/gen.git
-  GIT_TAG        origin/develop
+  GIT_TAG        6627fb009ddd9a8909820f7169d06f567f36077b # fix commit
 )
 # Building gen as dynamic library
 set(GEN_STATIC OFF)
