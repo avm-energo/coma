3a0e2bd54a0df546cee99bb93199c6502015f57a
vladd12 <vladd12z32@gmail.com>
2024-03-25 13:53:18 +0300

    [WIP] Reading osc files:
    [*] Update for receiving osc file
    [-] Removing useless code
    [*] Small changes

--- src/oscillograms/osckivdialog.cpp
+++ src/oscillograms/osckivdialog.cpp
@@ -2,9 +2,12 @@
 
 #include "../widgets/epopup.h"
 #include "../widgets/wd_func.h"
+#include "osc_ids.h"
 
 #include <device/current_device.h>
+#include <gen/files.h>
 #include <gen/stdfunc.h>
+#include <gen/timefunc.h>
 #include <limits>
 
 constexpr std::string_view oscFilenumLblFmt { "Текущий номер осциллограммы: %1" };
@@ -86,6 +89,22 @@ void OscKivDialog::enableButtons(const bool enabling)
     m_readBtn->setEnabled(enabling);
 }
 
+QString OscKivDialog::getFilename()
+{
+    QString filename("%1-%2-%3-%4.osc");
+    QString devType("%1");
+    devType = devType.arg(m_device->getDeviceType(), 4, 16, QChar('0'));
+    filename = filename.arg(m_device->getUID(), devType, QString::number(m_oscFilenum));
+    filename = filename.arg(TimeFunc::UnixTime64ToInvStringFractional(m_timestamp));
+    return filename;
+}
+
+void OscKivDialog::resetState()
+{
+    m_state = State::Init;
+    enableButtons(true);
+}
+
 void OscKivDialog::reqOscState()
 {
     m_device->async()->reqBitStrings(oscStateAddr, 1);
@@ -113,15 +132,51 @@ void OscKivDialog::updateGeneralResponse(const DataTypes::GeneralResponseStruct 
         auto errorMsg = //
             QString("Произошла ошибка: %1")
                 .arg(Error::MsgStr.value(static_cast<Error::Msg>(response.data), "Неизвестная ошибка"));
-        m_state = State::Init;
-        enableButtons(true);
         EMessageBox::error(this, errorMsg);
+        resetState();
     }
 }
 
 void OscKivDialog::receiveOscFile(const S2::FileStruct &file)
 {
-    ;
+    if (m_state != State::RequestFile)
+        return;
+
+    if (file.ID == MT_HEAD_ID)
+    {
+        auto header = m_manager.loadCommon(file);
+        m_timestamp = header.time;
+        m_manager.setHeader(header);
+    }
+    else
+    {
+        m_oscModel = m_manager.load(file);
+        if (!m_oscModel)
+        {
+            EMessageBox::error(this, "Ошибка чтения\nфайла осциллограммы");
+            resetState();
+            return;
+        }
+        m_manager.loadOsc(m_oscModel.get());
+    }
+
+    m_fileBuffer.push_back(file);
+    if (m_fileBuffer.size() == 2)
+    {
+        // Сохраняем в файл
+        QByteArray ba;
+        S2Util::StoreDataMem(ba, m_fileBuffer, m_oscFilenum);
+        auto filename = getFilename();
+        auto filepath = WDFunc::ChooseFileForSave(this, "Oscillograms (*.osc)", "osc", filename);
+        if (!filepath.isEmpty())
+        {
+            if (Files::SaveToFile(filepath, ba) == Error::Msg::NoError)
+                EMessageBox::information(this, "Записано успешно!");
+            else
+                EMessageBox::error(this, "Не удалось записать файл");
+        }
+        resetState();
+    }
 }
 
 void OscKivDialog::writeTypeOsc()
@@ -138,15 +193,9 @@ void OscKivDialog::writeTypeOsc()
 void OscKivDialog::reqOscFile()
 {
     if (m_oscFilenum == noOsc)
-    {
-        EMessageBox::error(this, "Невозможно прочитать файл,\nидёт запись осциллограммы");
-        return;
-    }
+        EMessageBox::error(this, "Запрашиваемый файл отсутствует");
     else if (m_oscFilenum == recordingOsc)
-    {
         EMessageBox::error(this, "Невозможно прочитать файл,\nидёт запись осциллограммы");
-        return;
-    }
     else
     {
         enableButtons(false);
--- src/oscillograms/osckivdialog.h
+++ src/oscillograms/osckivdialog.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../widgets/udialog.h"
+#include "oscmanager.h"
 
 class QLabel;
 class QPushButton;
@@ -27,22 +28,40 @@ private:
         RequestFile,
     };
 
+    OscManager m_manager;
+    std::unique_ptr<TrendViewModel> m_oscModel;
+    std::vector<S2::FileStruct> m_fileBuffer;
+    quint64 m_timestamp;
+
     QPushButton *m_commandBtn, *m_readBtn;
     QLabel *m_oscFilenumLbl;
     QTimer *m_reqStateOscTimer;
     u32 m_oscFilenum, m_oldOscFilenum;
     State m_state;
 
+    /// \brief Создаёт UI диалога.
     void setupUI();
+    /// \brief Обновление текущего номера осциллограммы.
     void updateOscFilenum(const u32 newOscFilenum);
+    /// \brief Включает и выключает кнопки управления на UI.
     void enableButtons(const bool enabling);
 
+    QString getFilename();
+
+    void resetState();
+
+    /// \brief Запрашивает статус записи осциллограмы.
     void reqOscState();
+    /// \brief Перегрузка для получения статуса записи осциллограммы.
     void updateBitStringData(const DataTypes::BitStringStruct &bs) override;
+    /// \brief Перегрузка для получения ошибок связи, etc...
     void updateGeneralResponse(const DataTypes::GeneralResponseStruct &response) override;
+    /// \brief Принимает части файла осциллограммы в формате S2.
     void receiveOscFile(const S2::FileStruct &file);
 
+    /// \brief Посылает команду записи осциллограммы с указанными из UI параметрами.
     void writeTypeOsc();
+    /// \brief Запрашивает файл осциллограммы после окончания записи осциллограммы.
     void reqOscFile();
 
 public:
