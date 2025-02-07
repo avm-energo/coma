6fa4ae2cc98430422c74517d4dde5c2f1b2b261c
Anton <vao@asu-vei.ru>
2025-02-07 13:37:20 +0300

    [!] temporary commit for debugging journals by protocom

--- src/interfaces/src/parsers/protocom_request_parser.cpp
+++ src/interfaces/src/parsers/protocom_request_parser.cpp
@@ -105,10 +105,10 @@ QByteArray ProtocomRequestParser::parse(const CommandStruct &cmd)
         case FilesEnum::JourSys:
         case FilesEnum::JourWork:
         case FilesEnum::JourMeas:
-#if defined(Q_OS_WINDOWS)
-            setExceptionalSituationStatus(true);
-            break;
-#endif
+            // #if defined(Q_OS_WINDOWS)
+            //             setExceptionalSituationStatus(true);
+            //             break;
+            // #endif
         default:
             m_request = prepareBlock(Proto::Commands::ReadFile, StdFunc::toByteArray(cmd.arg1.value<quint16>()));
             m_continueCommand = createContinueCommand(Proto::Commands::ReadFile);
