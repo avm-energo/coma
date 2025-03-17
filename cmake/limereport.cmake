2fae8cecf38061178f8530be1ca5d07fbd359d49
Anton <vao@asu-vei.ru>
2025-03-17 14:04:43 +0300

    [*] cmake files gt -> git

--- cmake/limereport.cmake
+++ cmake/limereport.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 
 FetchContent_Declare(limereport
-  GIT_REPOSITORY    https://gt.avmenergo.ru/avm-energo/limereport.git
+  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/limereport.git
   GIT_TAG           master
 )
 
