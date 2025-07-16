dc84bdb6c70bc1785869b2cf0eb022813e179485
Anton <vao@asu-vei.ru>
2025-07-16 21:37:43 +0300

    Update gen.cmake

--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(avm-gen
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/avm-gen.git
-  GIT_TAG           v1.9.18
+  GIT_TAG           v1.10.0
 )
 
 FetchContent_MakeAvailable(avm-gen)
