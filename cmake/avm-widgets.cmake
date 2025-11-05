1680661e84b8baf1efbfed7e41aedb23a2ba7507
anton <vao@asu-vei.ru>
2025-11-05 18:01:36 +0300

    [*] v3.4.2

--- cmake/avm-widgets.cmake
+++ cmake/avm-widgets.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(avm-widgets
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/avm-widgets.git
-  GIT_TAG           develop
+  GIT_TAG           v2.3.0
 )
 
 FetchContent_MakeAvailable(avm-widgets)
