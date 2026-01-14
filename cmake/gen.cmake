9c93e06c7cc758435a7157cf3c43b2f37dadc2f2
anton <vao@asu-vei.ru>
2026-01-14 15:16:52 +0300

    [+] include gen [!*] foolish mistake: all the sources and headers was included because of GLOB_RECURSE(PROJECT_SOURCE_DIR) instead of CMAKE_CURRENT_SOURCE_DIR

---
+++ cmake/gen.cmake
@@ -0,0 +1,8 @@
+include(FetchContent)
+
+FetchContent_Declare(avm-gen
+  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/avm-gen.git
+  GIT_TAG           v1.10.10
+)
+
+FetchContent_MakeAvailable(avm-gen)
