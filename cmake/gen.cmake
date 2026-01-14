728083d261fd22097d7ba8daa81fc8c959275c8f
anton <vao@asu-vei.ru>
2026-01-14 09:39:32 +0300

    [*] gen is fixed to 1.10.10

--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(avm-gen
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/avm-gen.git
-  GIT_TAG           develop
+  GIT_TAG           v1.10.10
 )
 
 FetchContent_MakeAvailable(avm-gen)
