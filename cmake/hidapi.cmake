c45cee70167426de70c680bc2bdbcd5637798824
anton <vao@asu-vei.ru>
2026-01-14 12:45:38 +0300

    [*] specify subprojects versions

--- cmake/hidapi.cmake
+++ cmake/hidapi.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(hidapi
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/hidapi.git
-  GIT_TAG           master
+  GIT_TAG           v1.0.0
   # SOURCE_SUBDIR     windows
 )
 
--- cmake/limereport.cmake
+++ cmake/limereport.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(limereport
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/limereport.git
-  GIT_TAG           master
+  GIT_TAG           v1.7.19
 )
 
 FetchContent_MakeAvailable(limereport)
