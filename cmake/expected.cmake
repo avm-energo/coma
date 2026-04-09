5ebd59f2bd2808bab2067874cb64cc1b8915a395
anton <vao@asu-vei.ru>
2026-04-09 14:08:11 +0300

    Update expected.cmake

--- cmake/expected.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(expected
-  GIT_REPOSITORY                https://git.asu-vei.ru/avm-energo/tl-expected.git
-  GIT_TAG                       master
-  DOWNLOAD_EXTRACT_TIMESTAMP    TRUE
-)
-
-FetchContent_MakeAvailable(expected)
