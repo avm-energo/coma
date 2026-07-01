d53634b27eed3ad912e9bce4ffea22ce7954cc2a
Michael <sanderland@silenthill.com>
2026-07-01 03:29:13 +0300

    add icon

--- src/avm-autobatch/main.cpp
+++ src/avm-autobatch/main.cpp
@@ -1,9 +1,15 @@
+#include "../abatcher/include/abatcher.h"
+
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(":/icons/coma.ico"));
-    
+    app.setWindowIcon(QIcon(":/icons/abatcher.ico"));
+
+    Abatcher abatcher;
+    abatcher.run();
+
     return app.exec();
 }
