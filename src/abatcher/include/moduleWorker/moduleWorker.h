e20112b960a0c8eb4f85aa25e9237cf52bb5c16d
Michael <sanderland@silenthill.com>
2026-07-03 20:10:03 +0300

    Already done

--- src/abatcher/include/moduleWorker/moduleWorker.h
+++ src/abatcher/include/moduleWorker/moduleWorker.h
@@ -6,9 +6,13 @@
 #include <interfaces/ifaces/usbhidport.h>
 #include <s2/filestruct.h>
 
+#include <QCheckBox>
+#include <QList>
 #include <QPushButton>
 #include <QTemporaryDir>
 #include <QWidget>
+
+class DataBlock;
 
 class ModuleWorker : public QWidget
 {
@@ -26,11 +30,15 @@ private:
     Device::CurrentDevice *m_device = nullptr;
     QTemporaryDir m_tempDir; // хранит скачанные файлы до финальной упаковки в ZIP (см. doc/abatcher-task.md, п.17)
     QString m_zipFileName;
+    QList<DataBlock *> m_bacBlocks; // настроечные блоки (Bac), состав зависит от типа модуля (см. collectBacBlocks)
+    bool m_hasStartup = false; // блок начальных значений реализован только для АВМ-КИВ (см. createStartupProgressBar)
+    bool m_hasFwUpdate = false; // анализ/принудительное обновление ВПО реализовано только для АВМ-КИВ (см. createFwUpdateProgressBar)
 
     QPushButton *m_closeButton = nullptr;
     QPushButton *m_startButton = nullptr;
     QPushButton *m_cancelButton = nullptr;
     QWidget *m_progressContainer = nullptr; // сюда добавляются прогресс-бары следующих шагов пакетника
+    QCheckBox *m_measJourCheckBox = nullptr; // позволяет пользователю отказаться от скачивания журнала измерений
     bool m_isCancelled = false;
 
     void setupUI();
@@ -38,7 +46,8 @@ private:
     void createStartButton();
     void createCancelButton();
     void createProgressBars();
-    
+    void createMeasJournalRow();
+
     bool connectToUSB();
     void loadXML();
     void startDownload();
@@ -49,6 +58,20 @@ private:
     void requestConfig();
     void saveJournals();
     void saveJournal(const QString &prbName, S2::FilesEnum fileNum, const QString &filename);
+    void collectBacBlocks();
+    void createBacProgressBars();
+    void saveBacBlocks();
+    void saveBacBlock(int index);
+    void createStartupProgressBar();
+    void saveStartup();
+    void createFwUpdateProgressBar();
+    void checkFirmwareVersion();
+    void updateFirmware(const QString &oldVerStr, const QString &newVerStr);
+    void createConfigCheckProgressBar();
+    void checkConfigParams();
+    void updateConfigParams();
+    void verifyConfigParams();
+    Error::Msg rereadConfigBlocking();
 
 private slots:
     void bsiInitFinished(Error::Msg status);
