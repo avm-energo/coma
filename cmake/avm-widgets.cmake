ed93e2e8bae65c6ba12994749c5fffb80f2f5ea5
anton <vao@asu-vei.ru>
2026-07-07 16:05:51 +0300

    [*] ext repos from ssh to https

--- cmake/avm-widgets.cmake
+++ cmake/avm-widgets.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 
 FetchContent_Declare(libavm-widgets
-  GIT_REPOSITORY    ssh://git@git.avmenergo.ru:5507/avm-energo/libavm-widgets.git
+  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-widgets.git
   GIT_TAG           elabel-copyable
 )
 
--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 
 FetchContent_Declare(libavm-gen
-  GIT_REPOSITORY    ssh://git@git.avmenergo.ru:5507/avm-energo/libavm-gen.git
+  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-gen.git
   GIT_TAG           main
 )
 
