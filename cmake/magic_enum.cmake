398cd6d4cc0d849aba9bd4644c7d5db3379f6ae7
anton <vao@asu-vei.ru>
2026-07-23 14:03:30 +0300

    [+] better_enums

--- cmake/magic_enum.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(magic-enum
-  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-magic_enum.git
-  GIT_TAG           master
-)
-
-FetchContent_MakeAvailable(magic-enum)
