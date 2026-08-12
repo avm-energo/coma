532102b87d5c8d2046d719b99bb2178ce753627f
anton <vao@asu-vei.ru>
2026-08-12 15:58:43 +0300

    [*] hidapi call moved into interfaces CMakeLists [*] hidapi is now libavm-hidapi, the corresponding changes has been made [^] dependencies list update

--- cmake/hidapi.cmake
+++ cmake/hidapi.cmake
@@ -1,9 +1,9 @@
 include(FetchContent)
 
-FetchContent_Declare(hidapi
-  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/hidapi.git
+FetchContent_Declare(libavm-hidapi
+  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-hidapi.git
   GIT_TAG           main
   # SOURCE_SUBDIR     windows
 )
 
-FetchContent_MakeAvailable(hidapi)
+FetchContent_MakeAvailable(libavm-hidapi)
