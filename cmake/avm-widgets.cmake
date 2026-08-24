7f911d702e16c03a3806695437006f04572f70b2
Michael <sanderland@silenthill.com>
2026-08-24 15:11:25 +0300

    Fix metrology in KIV

--- cmake/avm-widgets.cmake
+++ cmake/avm-widgets.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(libavm-widgets
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-widgets.git
-  GIT_TAG           ww-refactoring
+  GIT_TAG           main
 )
 
 FetchContent_MakeAvailable(libavm-widgets)
