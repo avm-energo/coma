cafac0455f2e700919cdd08bb293dc50ee63a6ff
anton <vao@asu-vei.ru>
2026-08-24 13:11:48 +0300

    [*] cmake includes wrong url fix

--- cmake/better_enums.cmake
+++ cmake/better_enums.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 
 FetchContent_Declare(better_enums
-  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/better-enums.git
+  GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-better-enums.git
   GIT_TAG           master
 )
 
