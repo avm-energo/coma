1fe15907d7fe292872b214c1a0d9468ffb30184f
Michael <sanderland@silenthill.com>
2026-08-27 14:59:54 +0300

    switch git branch gen

--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(libavm-gen
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-gen.git
-  GIT_TAG           setConfig
+  GIT_TAG           main
 )
 
 FetchContent_MakeAvailable(libavm-gen)
