6cbc0c95c85f2a6b0b16f4379afca715277f71a1
Michael <sanderland@silenthill.com>
2026-09-21 18:01:08 +0300

    minor

--- cmake/gen.cmake
+++ cmake/gen.cmake
@@ -2,8 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(libavm-gen
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libavm-gen.git
-  GIT_TAG           RewriteDrivesFunc
-
+  GIT_TAG           main
 )
 
 FetchContent_MakeAvailable(libavm-gen)
